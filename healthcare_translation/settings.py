7201aa7bda79e11edd962902cabfa9e86de5f098
Fareed04 <ologundudufareed@gmail.com>
2025-03-30 18:55:57 +0100

    Fix static files on Vercel

--- healthcare_translation/settings.py
+++ healthcare_translation/settings.py
@@ -135,10 +135,6 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "translation_app", "static"),
-]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'translation_app/static')]
