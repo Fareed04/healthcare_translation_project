7ba0e075188174cda94ce02da26722b489ce72b6
Fareed04 <ologundudufareed@gmail.com>
2025-03-30 17:52:11 +0100

    Fixed static file duplication issue

--- healthcare_translation/settings.py
+++ healthcare_translation/settings.py
@@ -125,7 +125,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -133,7 +132,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = "/static/"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "translation_app", "static")]
 STATICFILES_DIRS = [BASE_DIR / "translation_app/static"]
